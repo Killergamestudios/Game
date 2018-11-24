ca24b58ab135053c07e6415b084258da7ecc9d45
tony <a.valmas@space-innovation.org>
2018-11-24 18:31:31 +0200

    Adds A* star algorithms (pathfinding and range finder)

--- GameApp/Algorithms2.cpp
+++ GameApp/Algorithms2.cpp
@@ -10,9 +10,11 @@ typedef pair<int, pair<int, int>> pPair;
 
 typedef pair<int, int> Pair;
 
-vector<pair<int, int>> reconstructPath(map<pair<int, int>, pair<int, int>> cameFrom, int endPosX, int endPosY) {
-	vector<pair<int, int>> totalPath;
-	totalPath.push_back(make_pair(endPosX, endPosY));
+
+// function to reconstruct the path form getPath function
+vector<Vector2i> reconstructPath(map<pair<int, int>, pair<int, int>> cameFrom, int endPosX, int endPosY) {
+	vector<Vector2i> totalPath;
+	totalPath.push_back(Vector2i(endPosX, endPosY));
 	int x = endPosX;
 	int y = endPosY;
 	while (cameFrom.find(make_pair(x, y)) != cameFrom.end())
@@ -20,12 +22,13 @@ vector<pair<int, int>> reconstructPath(map<pair<int, int>, pair<int, int>> cameF
 		Pair next = cameFrom[make_pair(x, y)];
 		x = next.first;
 		y = next.second;
-		totalPath.push_back(make_pair(x, y));
+		totalPath.push_back(Vector2i(x, y));
 	}
 
 	return totalPath;
 }
 
+// construct totalMap using the layers provided by Map class
 int ** getTotalMap(Map currentMap) {
 	int ** terrainMap = currentMap.getTerrainMap();
 	int ** enemyMap = currentMap.getEnemyMap();
@@ -49,76 +52,150 @@ int ** getTotalMap(Map currentMap) {
 
 	return map;
 }
+
+// function that estimates the distance between two tiles (can be optimised)
 int heuristicCostEstimate(int startPosX, int startPosY, int endPosX, int endPosY)
 {
 	return abs(startPosX - endPosX) + abs(startPosY - endPosY);
 }
 
-vector<pair<int, int>> getPath(int startPosX, int startPosY, int endPosX, int endPosY, Map currentMap) 
+// function to get the optimal path between (startX, startY) and (endX, endY)
+vector<Vector2i> getPath(int startPosX, int startPosY, int endPosX, int endPosY, Map currentMap)
 {
 	pPair start = make_pair(heuristicCostEstimate(startPosX, startPosY, endPosX, endPosY), make_pair(startPosX, startPosY));
-	set<pPair> openSet;
+	set<pPair> openSet; // tiles that are to be processed
+	map<pair<int, int>, bool> closedSet; // tiles that have already been processed
+	map<pair<int,int>,pair<int,int>> cameFrom; // list keeping track of the optimal path
+	int ** gScore;  // the weight to get to a certain node from the start
+	int ** totalMap = getTotalMap(currentMap); // map containing all the weights and blocked tiles
+	int mapWidth = currentMap.getMapWidth(); // width of map array
+	int mapHeight = currentMap.getMapHeight(); // height of map array
+
 	openSet.insert(start);
-	map<pair<int, int>, bool> closedSet;
-	map<pair<int,int>,pair<int,int>> cameFrom;
-	int ** gScore;
-	gScore = new int *[currentMap.getMapWidth()];
-	for (int i = 0; i < currentMap.getMapWidth(); i++)
+	// initialize gscore
+	gScore = new int *[mapWidth]; 
+	for (int i = 0; i < mapWidth; i++)
 	{
-		gScore[i] = new int[currentMap.getMapHeight()];
-		for (int j = 0; j < currentMap.getMapHeight(); j++)
+		gScore[i] = new int[mapHeight];
+		for (int j = 0; j < mapHeight; j++)
 		{
 			gScore[i][j] = INT32_MAX;
 		}
 	}
-	gScore[startPosX][startPosY] = 0;
-	set<pPair> fScore;
-	fScore.insert(start);
-	
-	int ** totalMap = getTotalMap(currentMap);
+	gScore[startPosX][startPosY] = 0; // gscore(start) = 0
 	
 	while (openSet.size() != 0) 
 	{
-		pPair p = *openSet.begin();
-		int x = p.second.first;
-		int y = p.second.second;
-		if (x == endPosX && y == endPosY) {
+		pPair p = *openSet.begin(); // pop first item from openSet (this with the least weight)
+		int x = p.second.first;     //
+		int y = p.second.second;    // get its coordinates
+		if (x == endPosX && y == endPosY) { // check if we found the end
 			return reconstructPath(cameFrom,endPosX,endPosY);
 		}
-		openSet.erase(openSet.begin());
-		closedSet[make_pair(x,y)] = true;
+		openSet.erase(openSet.begin()); // erase first item from openSet (this with the least weight)
+		closedSet[make_pair(x,y)] = true; // mark as evaluated to its optimal value
 
+		// create the abjucent tiles (x+1,y) (x-1,y) (x,y+1) (x,y-1) 
 		for (int i = x - 1; i <= x + 1; i+=2)
 		{
-			if (closedSet[make_pair(i, y)] == true)
+			if (i < 0 || i > mapWidth || closedSet[make_pair(i, y)] == true || totalMap[i][y] == -1) // blocked or already "closed" tiles
 				continue;
-			int score = gScore[x][y] + totalMap[i][y];
 
-			if (gScore[i][y] == INT32_MAX)
+			int score = gScore[x][y] + totalMap[i][y]; //calculate new score = estimation + weight of tile
+
+			if (gScore[i][y] == INT32_MAX) // if never visited before add it to openSet
 				openSet.insert(make_pair(score + heuristicCostEstimate(i, y, endPosX, endPosY), make_pair(i, y)));
-			else if (score >= gScore[i][y])
+			else if (score >= gScore[i][y]) // if score is worse than the already registered, continue
 				continue;
 			
-			cameFrom[make_pair(i,y)] = make_pair(x,y);
+			// add tile to the list. Lower weight found!
+			cameFrom[make_pair(i,y)] = make_pair(x,y); 
 			gScore[i][y] = score;
-			fScore.insert(make_pair(score + heuristicCostEstimate(i, y, endPosX, endPosY), make_pair(i, y)));
 		}
 
 		for (int j = y - 1; j <= y + 1; j += 2)
 		{
-			if (closedSet[make_pair(x, j)] == true)
+			if (j < 0 || j > mapHeight || closedSet[make_pair(x, j)] == true || totalMap[x][j] == -1) // blocked or already "closed" tiles
 				continue;
-			int score = gScore[x][y] + totalMap[x][j];
+			int score = gScore[x][y] + totalMap[x][j]; //calculate new score = estimation + weight of tile
 
-			if (gScore[x][j] == INT32_MAX)
+			if (gScore[x][j] == INT32_MAX) // if never visited before add it to openSet
 				openSet.insert(make_pair(score + heuristicCostEstimate(x, j, endPosX, endPosY), make_pair(x, j)));
-			else if (score >= gScore[x][j])
+			else if (score >= gScore[x][j]) // if score is worse than the already registered, continue
 				continue;
 
+			// add tile to the list. Lower weight found!
 			cameFrom[make_pair(x, j)] = make_pair(x, y);
 			gScore[x][j] = score;
-			fScore.insert(make_pair(score + heuristicCostEstimate(x, j, endPosX, endPosY), make_pair(x, j)));
 		}
-
 	}
 }
+
+// function to find all the tiles you can access given a starting point and a range 
+vector<Vector2i> getAllAvailableTiles(int startPosX, int startPosY, int range, Map currentMap)
+{
+	pPair start = make_pair(0, make_pair(startPosX, startPosY)); //the starting point and its distance from the beginning = 0
+	Pair startW = make_pair(startPosX, startPosY); // the starting point without weight
+	map<pair<int, int>, bool> closedSet; // tiles that have already been processed
+	vector<pPair> openSet; // tiles that are to be processed
+	vector<Vector2i> finalTilesSet; // the final set of tiles to return
+	set<pair<int, int>> tiles; // array keeping track of tiles. Needs to be set in order not to accept duplicates
+	int mapWidth = currentMap.getMapWidth(); // width of map array
+	int mapHeight = currentMap.getMapHeight(); // height of map array
+
+	closedSet[startW] = false;
+	openSet.push_back(start);
+	tiles.insert(startW);
+	int ** totalMap = getTotalMap(currentMap);
+
+	while (openSet.size() != 0)
+	{
+		pPair p = *openSet.begin(); // pop first item from openSet (this with the least weight)
+		int x = p.second.first;  //
+		int y = p.second.second; // get its coordinates
+		int weight = p.first; // get the distance to this tile
+		openSet.erase(openSet.begin()); // erase first item from set
+		closedSet[make_pair(x, y)] = true; // mark as evaluted to its optimal value
+
+		// create the abjucent tiles (x+1,y) (x-1,y) (x,y+1) (x,y-1) 
+		for (int i = x - 1; i <= x + 1; i += 2)
+		{
+			// if outOfbound or blocked continue
+			if (i < 0 || i > mapWidth - 1 || totalMap[i][y] == -1)
+				continue;
+			int newWeight = totalMap[i][y] + weight; // evaluate its distance
+			if (closedSet[make_pair(i, y)] || newWeight > range) // if outOfRange ignore
+				continue;
+			tiles.insert(make_pair(i, y)); // insert the tile because it is in range
+			if (newWeight < range) // if we can go further than that add it to the openSet
+			{
+				openSet.push_back(make_pair(newWeight, make_pair(i, y)));
+			}
+		}
+
+		for (int j = y - 1; j <= y + 1; j += 2)
+		{
+			// if outOfbound or blocked continue
+			if (j < 0 || j > mapHeight - 1 || totalMap[x][j] == -1)
+				continue;
+			int newWeight = totalMap[x][j] + weight; // evaluate its distance
+			if (closedSet[make_pair(x, j)] || newWeight > range) // if outOfRange ignore
+				continue;
+			tiles.insert(make_pair(x, j)); // insert the tile because it is in range
+			if (newWeight < range) // if we can go further than that add it to the openSet
+			{
+				openSet.push_back(make_pair(newWeight, make_pair(x, j)));
+			}
+
+		}
+	}
+
+	// create finalTilesSet in order to return a vector and not a set
+	for (Pair p : tiles)
+	{
+		finalTilesSet.push_back(Vector2i(p.first,p.second));
+	}
+	return finalTilesSet;
+}
+
+
