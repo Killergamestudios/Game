3f66ada05e5fe52040fb8b75f7f8268149a88084
Panagiotis Loulakis <panloulakis@gmail.com>
2018-12-19 16:29:25 +0200

    drow update

--- GameApp/draw.cpp
+++ GameApp/draw.cpp
@@ -11,6 +11,9 @@ void Engine::draw() {
 		break;
 	case State::Playing:
 		m_map->draw();
+		for (unsigned int i = 0; i < party.size(); i++) {
+			party[i].Draw();
+		}
 		break;
 	case State::Loading:
 		break;
@@ -19,4 +22,6 @@ void Engine::draw() {
 	}
 	m_window.setView(camera);
 	m_window.display();
+
+	
 }
