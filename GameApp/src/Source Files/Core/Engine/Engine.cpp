2f49edb58a3b37a03e196ba81a3418f43e4a906a
Josephkhland <Josephkhland@gmail.com>
2019-03-09 19:06:14 +0200

    Added some comments in the MouseControl Function to know might be needed later

--- GameApp/src/Source Files/Core/Engine/Engine.cpp
+++ GameApp/src/Source Files/Core/Engine/Engine.cpp
@@ -54,18 +54,19 @@ void Engine::updateState()
 
 void Engine::mouseControl(float &totalTimePassed, int &animState)
 {
-	float animationSpeed = 0.1f;
-	if (totalTimePassed  > animationSpeed)
+	//Mouse control should be adapted to work differently depending on the state of the game. Or if an in-game menu is open it should affect the menu or not work at all.
+	//Should also be able to use it for selecting where to move. It might prove useful to make this function return a value. Either return an int Value depending on the type of object it traces below the mouse,
+	//or it could be returning the coordinates within the map arrays.
+
+	int tileContent=0; // 0 For Empty, 1 for Friendly in Tile, 2 for Enemy in Tile, 3 for Misc in tile.
+	float animationSpeed = 0.1f; // Animation speed
+	if (totalTimePassed  > animationSpeed) //Animation
 	{
 		totalTimePassed=0;
 		animState = (animState + 64) % 1024; //Has 16 frames of 64 pixel.
 	
 	}
 
-	if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-	{
-		// left click...
-	}
 	// get global mouse position
 	sf::Vector2i position = sf::Mouse::getPosition(m_window);
 
@@ -84,16 +85,30 @@ void Engine::mouseControl(float &totalTimePassed, int &animState)
 	else if (Controller::getMap()->getFriendlyinPosition(localPos) > 0)
 	{
 		tile_sprite.setTextureRect(sf::IntRect(animState, 64, 64, 64)); // If friendly is on a tile, set the color of the target selector to yellow.
+		tileContent = 1; //Friendly in tile
 	}
 	else if (Controller::getMap()->getEnemyinPosition(localPos) > 0)
 	{
 		tile_sprite.setTextureRect(sf::IntRect(animState, 128, 64, 64)); //If enemy is on a tile, set the color of the target selector to red.
+		tileContent = 2; //Enemy in tile
 	}
 	else
 	{
 		tile_sprite.setTextureRect(sf::IntRect(animState, 0, 64, 64)); //If there is nothing set the color of the target selector to blue.
 	}
 	
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+	{
+		// left click...
+		if (tileContent == 1)
+		{
+			//Control the character through in-game menu, disable mouseControl.
+		}
+		else if (tileContent == 2)
+		{
+			//Show up information about the target 
+		}
+	}
 }
 
 
