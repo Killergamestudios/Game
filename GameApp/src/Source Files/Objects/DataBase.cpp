f75ca868c241b80df7a5967885a3ee9833a288e5
Josephkhland <Josephkhland@gmail.com>
2019-03-14 02:08:17 +0200

    added getCharacter function in Database. It stills needs more.

--- GameApp/src/Source Files/Objects/DataBase.cpp
+++ GameApp/src/Source Files/Objects/DataBase.cpp
@@ -43,6 +43,73 @@ WeaponComponent * DataBase::getWeapon(string Name)
   throw "404: not found";
   return nullptr;
 }
+CharacterObject DataBase::getCharacter(int ID, RenderWindow & window, String Category, String Type, Vector2i Position, Texture & texture, int level)
+{
+	int id = ID;
+	string Name = "nullCharacter";
+	for (auto &const it : charactersIndex)
+	{
+		if (it.second == id)
+		{
+			Name = it.first;
+			break;
+		}
+	}
+	Stats temp;
+	temp = statBlocks[id];
+	temp.Agility += level * statGainBlocks[id].AgilityGain / 5;
+	temp.MaxActions += level * statGainBlocks[id].MaxActionsGain;
+	temp.MaxEnergy += level * statGainBlocks[id].MaxEnergyGain;
+	temp.MaxHealth += level * statGainBlocks[id].MaxHealthGain;
+	temp.Precision += level * statGainBlocks[id].PrecisionGain;
+
+	temp.Health = temp.MaxHealth;
+	temp.MaxAgility = temp.Agility;
+	temp.MaxPrecision = temp.Precision;
+	temp.Energy = temp.MaxEnergy;
+	temp.MaxActions = temp.actionsremaining;
+	temp.level = level;
+
+	CharacterObject  newCharacter(Name, window, Category, Type, Position, texture);
+	newCharacter.equipWeapon(getWeapon(characterAttachments[id][0]));
+	newCharacter.equipArmor(getArmor(characterAttachments[id][1]));
+	// Somehow includes ability 1 (characterAttachments[id][2])
+	// Somehow includes ability 2 (characterAttachments[id][3])
+	// Somehow includes ability 3 (characterAttachments[id][4])
+
+
+
+	return newCharacter;
+}
+CharacterObject  DataBase::getCharacter(string Name, RenderWindow & window, String Category, String Type, Vector2i Position, Texture & texture,int level)
+{
+	int id = charactersIndex.at(Name);
+	Stats temp;
+	temp = statBlocks[id];
+	temp.Agility += level * statGainBlocks[id].AgilityGain / 5;
+	temp.MaxActions += level * statGainBlocks[id].MaxActionsGain;
+	temp.MaxEnergy += level * statGainBlocks[id].MaxEnergyGain;
+	temp.MaxHealth += level * statGainBlocks[id].MaxHealthGain;
+	temp.Precision += level * statGainBlocks[id].PrecisionGain;
+
+	temp.Health = temp.MaxHealth;
+	temp.MaxAgility = temp.Agility;
+	temp.MaxPrecision = temp.Precision;
+	temp.Energy = temp.MaxEnergy;
+	temp.MaxActions = temp.actionsremaining;
+	temp.level = level;
+
+	CharacterObject  newCharacter(Name, window, Category, Type, Position, texture);
+	newCharacter.equipWeapon(getWeapon(characterAttachments[id][0]));
+	newCharacter.equipArmor(getArmor(characterAttachments[id][1]));
+	// Somehow includes ability 1 (characterAttachments[id][2])
+	// Somehow includes ability 2 (characterAttachments[id][3])
+	// Somehow includes ability 3 (characterAttachments[id][4])
+
+
+
+	return newCharacter;
+}
 DataBase::~DataBase()
 {
 }
